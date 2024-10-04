9badb099af30a2944004b47766065be912f134e3
emersonpessoa01 <eps011108@gmail.com>
2024-10-04 18:04:24 -0300

    fix: finished home :tada:

--- public/js/main.js
+++ public/js/main.js
@@ -6,29 +6,50 @@ menu.onclick = () => {
   navbarNav.classList.toggle("active");
 };
 
+// Função para buscar os dados da API
 const getJson = async () => {
   try {
-    let url = "http://localhost:3000/api/data";
+    let url = "/api/data";
     let response = await fetch(url);
-    let json = await response.json();
-    // console.log(json.produtos);
-    let produtos = json.produtos;
-    produtos.forEach(item=>{
-      // console.log(item)
-      let card = document.createElement("div");
-      card.className = "card";
-      card.innerHTML = `
-        <img src="${item.special_image}" class="card-img-top" alt="${item.name_client}">
-        <div class="card-body">
-          <h5 class="card-title">${item.name_client}</h5>
-          <p class="card-text">${item.description}</p>
-          <a href="#" class="btn btn-primary">Comprar</a>
-        </div>
-      `;
-      document.querySelector("#container-home").appendChild(card);
-    })
+    let { produtos } = await response.json(); // Assume que os produtos estão no array 'produtos'
+    console.log(produtos);
+    // Chama a função para renderizar os produtos
+    // renderProducts(produtos);
   } catch (error) {
     console.log(`Error: ${error.message}`);
   }
 };
+
+// Executa a função de busca de dados
 getJson();
+
+// Função para renderizar os produtos
+// const renderProducts = (produtos) => {
+//   const swiperWrapper = document.querySelector(".swiper-wrapper");
+//   produtos.forEach((produto) => {
+//     const getSlide = `
+//       <div class="swiper-slide">
+//         <div class="bg-indigo-50 rounded-2xl h-96 flex justify-center items-center">
+//           <img src="${produto.special_image}" alt="Slide 1" class="w-full h-full object-cover rounded-2xl" />
+//         </div>
+//       </div>
+//     `;
+//     swiperWrapper.innerHTML += getSlide;
+//   });
+// };
+
+let swiper = new Swiper(".default-carousel", {
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  autoplay: {
+    delay: 3000, // Tempo em milissegundos entre as transições
+    disableOnInteraction: false, // O autoplay não será desativado após a interação do usuário
+  },
+});
