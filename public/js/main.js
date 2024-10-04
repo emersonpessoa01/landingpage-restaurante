d825416d4a1d332a34f4d8df646fd2cc562cebc8
emersonpessoa01 <eps011108@gmail.com>
2024-10-04 10:01:48 -0300

    fix: adjusting getJson :wrench:

--- public/js/main.js
+++ public/js/main.js
@@ -11,17 +11,14 @@ const getJson = async () => {
     let url = "http://localhost:3000/api/data";
     let response = await fetch(url);
     let json = await response.json();
+    // console.log(json.produtos);
     let produtos = json.produtos;
-
-    produtos.forEach(item => {
+    produtos.forEach(item=>{
+      // console.log(item)
       let card = document.createElement("div");
       card.className = "card";
-      
-      // Transformando o caminho relativo em absoluto
-      let imagePath = `http://localhost:3000${item.special_image.replace('./', '/')}`;
-      
       card.innerHTML = `
-        <img src="${imagePath}" class="card-img-top" alt="${item.name_client}">
+        <img src="${item.special_image}" class="card-img-top" alt="${item.name_client}">
         <div class="card-body">
           <h5 class="card-title">${item.name_client}</h5>
           <p class="card-text">${item.description}</p>
@@ -29,11 +26,9 @@ const getJson = async () => {
         </div>
       `;
       document.querySelector("#container-home").appendChild(card);
-    });
+    })
   } catch (error) {
     console.log(`Error: ${error.message}`);
   }
 };
-
 getJson();
-
