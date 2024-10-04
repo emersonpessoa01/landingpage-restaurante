03090ebee9f198399e080fb7a2cf6427c98f834c
emersonpessoa01 <eps011108@gmail.com>
2024-10-04 09:55:43 -0300

    test: testing the image path :wrench:

--- public/js/main.js
+++ public/js/main.js
@@ -8,19 +8,32 @@ menu.onclick = () => {
 
 const getJson = async () => {
   try {
-    let url = `https://landingpage-ecommerce-git-master-emersonpessoa01s-projects.vercel.app/api/data`;
+    let url = "http://localhost:3000/api/data";
     let response = await fetch(url);
     let json = await response.json();
-    console.log(json.produtos);
+    let produtos = json.produtos;
 
-    // let home = renderHome(json.produtos);
-    // console.log(home);
-    // let containerHome = document.getElementById("container-home");
-    // containerHome.innerHTML = home;
+    produtos.forEach(item => {
+      let card = document.createElement("div");
+      card.className = "card";
+      
+      // Transformando o caminho relativo em absoluto
+      let imagePath = `http://localhost:3000${item.special_image.replace('./', '/')}`;
+      
+      card.innerHTML = `
+        <img src="${imagePath}" class="card-img-top" alt="${item.name_client}">
+        <div class="card-body">
+          <h5 class="card-title">${item.name_client}</h5>
+          <p class="card-text">${item.description}</p>
+          <a href="#" class="btn btn-primary">Comprar</a>
+        </div>
+      `;
+      document.querySelector("#container-home").appendChild(card);
+    });
   } catch (error) {
     console.log(`Error: ${error.message}`);
   }
 };
-getJson();
 
+getJson();
 
