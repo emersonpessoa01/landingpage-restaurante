c0dddc882c4cef91f38bba48af0fe79c831feb59
emersonpessoa01 <eps011108@gmail.com>
2024-10-16 10:02:08 -0300

    feat: added loader :sparkles:

--- public/js/main.js
+++ public/js/main.js
@@ -62,6 +62,13 @@ window.addEventListener("load", () => {
   });
 });
 
+/* Loader */
+function loader() {
+  document.querySelector(".loader-container").classList.add("fade-out");
+}
 
+function fadeOut() {
+  setInterval(loader, 3000);
+}
 
-
+window.onload = fadeOut();
